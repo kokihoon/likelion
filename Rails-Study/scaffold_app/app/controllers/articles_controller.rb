44f255c515f3cc596305ebdb2f9cfe08d2d46c0a
kokihoon <rhrlgns777@gmail.com>
2018-06-02 23:01:15 +0900

    scffold_2

---
+++ Rails-Study/scaffold_app/app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def new
+  end
+
+  def create
+    #binding.pry
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article  # redirect_to article_url(@article.id)
+
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
