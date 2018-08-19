ed0bc74c47136dfd487f69c8e71b396404b68227
kokihoon <rhrlgns777@gmail.com>
2018-08-19 15:53:28 +0900

    Complated Profile

--- relation_app/app/controllers/profiles_controller.rb
+++ relation_app/app/controllers/profiles_controller.rb
@@ -2,11 +2,13 @@ class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update]
   before_action :write_profile, except: [:new, :create]
   def new
+    redirect_to edit_profile_url if !current_user.profile.nil?
     @profile = Profile.new
   end
 
   def create
     @profile = Profile.new(profile_params)
+    @prifile.address = convert_address
     @profile.user = current_user
     # binding.pry
     if @profile.save
@@ -23,6 +25,7 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    @profile.address = convert_address
     if @profile.update(profile_params)
       redirect_to my_profile_url
     else
@@ -36,10 +39,18 @@ class ProfilesController < ApplicationController
   end
 
   def profile_params
-     params.require(:profile).permit(:name, :address, :mobile, :birth)
+     params.require(:profile).permit(:name, :mobile, :birth)
   end
 
   def write_profile
       redirect_to new_profile_url if @profile.nil?
+  end
+
+  def convert_address
+    address = ''
+    params[:profile][:address].each_value do |v|
+      address += "#{v}/ "
+    end
+    return address
   end
 end
--- relation_app/config/routes.rb
+++ relation_app/config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
 
   get '/my_profile', to: 'profiles#show'
   get '/new_profile', to: 'profiles#new'
+  get '/create_profile', to: 'profiles#new'
   post '/create_profile', to: 'profiles#create'
   get '/edit_profile', to: 'profiles#edit'
+  get '/update_profile', to: 'profiles#edit'
   patch '/update_profile', to: 'profiles#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
